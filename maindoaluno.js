1852caffaf493d65b5249694fa0b69fd26f06613
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-02-10 14:01:57 -0300

    Add files via upload

---
+++ maindoaluno.js
@@ -0,0 +1,10 @@
+//Crie as variveis 'img', 'status' 
+//Crie uma lista chamada 'objetos'
+???
+//Crie uma função que se chama preload
+//Dentro dessa função, você terá uma variável chamada 'img'
+//A variável 'img' irá receber o código 'loadImage' que irá receber uma imagem
+???
+//Crie a função 'setup' e dentro dela você irá criar um canva
+???
+//Deixe o seu canva no centro
